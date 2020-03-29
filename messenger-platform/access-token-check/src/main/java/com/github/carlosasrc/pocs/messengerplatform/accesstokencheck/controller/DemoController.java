a6ee285b1bbfd1578cb1ce3e729ec4d588cbdb22
CarlosAsrc <carlos.asrc@gmail.com>
2020-03-29 19:50:22 -0300

    Add creation of user access token and get request for page access token

--- messenger-platform/access-token-check/src/main/java/com/github/carlosasrc/pocs/messengerplatform/accesstokencheck/controller/DemoController.java
+++ messenger-platform/access-token-check/src/main/java/com/github/carlosasrc/pocs/messengerplatform/accesstokencheck/controller/DemoController.java
@@ -2,8 +2,6 @@ package com.github.carlosasrc.pocs.messengerplatform.accesstokencheck.controller
 
 import com.github.carlosasrc.pocs.messengerplatform.accesstokencheck.service.FacebookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,14 +22,9 @@ public class DemoController {
         facebookService.createFacebookAccessToken(code);
     }
 
-    @GetMapping("/getName")
-    public String getNameResponse(){
-        return facebookService.getName();
-    }
-
-    @GetMapping("/teste")
-    public ResponseEntity<String> teste(){
-        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    @GetMapping("/getPageAccessToken")
+    public String getPageAccessToken(){
+        return facebookService.getPageAccessToken();
     }
 
 }
--- messenger-platform/access-token-check/src/main/java/com/github/carlosasrc/pocs/messengerplatform/accesstokencheck/service/FacebookService.java
+++ messenger-platform/access-token-check/src/main/java/com/github/carlosasrc/pocs/messengerplatform/accesstokencheck/service/FacebookService.java
@@ -1,14 +1,17 @@
 package com.github.carlosasrc.pocs.messengerplatform.accesstokencheck.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.User;
 import org.springframework.social.facebook.api.impl.FacebookTemplate;
 import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.social.oauth2.AccessGrant;
 import org.springframework.social.oauth2.OAuth2Operations;
 import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @PropertySource("classpath:application.properties")
@@ -18,27 +21,34 @@ public class FacebookService {
     @Value("${spring.social.facebook.appSecret}")
     String facebookSecret;
 
-    private String accessToken;
+    private String userAccessToken;
+    private String userId;
 
+    @Autowired
+    private RestTemplate restTemplate;
 
     public String createFacebookAuthorizationURL(){
         FacebookConnectionFactory connectionFactory = new FacebookConnectionFactory(facebookAppId, facebookSecret);
         OAuth2Operations oauthOperations = connectionFactory.getOAuthOperations();
         OAuth2Parameters params = new OAuth2Parameters();
         params.setRedirectUri("http://localhost:8080/facebook");
-        params.setScope("public_profile,email,user_birthday");
+        params.setScope("public_profile,email,user_birthday,manage_pages,pages_show_list");
         return oauthOperations.buildAuthorizeUrl(params);
     }
 
     public void createFacebookAccessToken(String code) {
         FacebookConnectionFactory connectionFactory = new FacebookConnectionFactory(facebookAppId, facebookSecret);
         AccessGrant accessGrant = connectionFactory.getOAuthOperations().exchangeForAccess(code, "http://localhost:8080/facebook", null);
-        accessToken = accessGrant.getAccessToken();
+        userAccessToken = accessGrant.getAccessToken();
+
+        Facebook facebook = new FacebookTemplate(userAccessToken);
+        String[] fields = {"id", "name"};
+        User user = facebook.fetchObject("me", User.class, fields);
+        userId = user.getId();
     }
 
-    public String getName() {
-        Facebook facebook = new FacebookTemplate(accessToken);
-        String[] fields = {"id", "name"};
-        return facebook.fetchObject("me", String.class, fields);
+
+    public String getPageAccessToken() {
+        return restTemplate.getForObject("https://graph.facebook.com/"+ userId +"/accounts?access_token="+ userAccessToken,  String.class);
     }
 }
